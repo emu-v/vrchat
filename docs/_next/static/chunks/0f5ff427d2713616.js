c9431d8eaeda4b7659fb90bda2112e482af35ecd
emu-v <emu0virtual@gmail.com>
2025-10-23 05:41:41 +0900

    add worlds

--- docs/_next/static/chunks/0f5ff427d2713616.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/4eba83218c5c52cf.js",
-  "static/chunks/d38117955e035609.js",
-  "static/chunks/turbopack-5acdd3a3eb268682.js"
-])
--- docs/_next/static/chunks/11266547568f6578.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/816650f1c2d31d99.js",
-  "static/chunks/d38117955e035609.js",
-  "static/chunks/turbopack-9466b0588fcaf799.js"
-])
